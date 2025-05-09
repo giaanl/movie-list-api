84cba757b2ec21296133a343f4378c2f79e0af7c
Gianluca Paschoalotti <gianluca170703@gmail.com>
2025-05-09 20:09:22 -0300

    Melhoria criação de tabela

--- src/domain/entities/movie.entity.ts
+++ src/domain/entities/movie.entity.ts
@@ -1,6 +1,7 @@
-import { Column } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
+@Entity('movies')
 export class Movie extends BaseEntity {
   @Column({
     type: 'varchar',
